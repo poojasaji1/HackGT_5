33ddb3ad94212b58d68ab977400a575a920b94d4
poojasaji1 <psaji3@gatech.edu>
2024-09-28 00:43:48 -0400

    Updated Register Page

--- Leaf/views.py
+++ Leaf/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'Leaf/register.html', {'form': form})
+
 def home(request):
     return render(request, 'Leaf/home.html')
 
@@ -8,6 +22,3 @@ def about(request):
 
 def contact(request):
     return render(request, 'Leaf/contact.html')
-
-def register(request):
-    return render(request, 'Leaf/register.html')
