f34321ccf287c8fe11f8d66517886f4ba5cd3a41
poojasaji1 <psaji3@gatech.edu>
2024-09-28 00:31:33 -0400

    Initial commit

---
+++ Leaf/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'Leaf/home.html')
+
+def about(request):
+    return render(request, 'Leaf/about.html')
+
+def contact(request):
+    return render(request, 'Leaf/contact.html')
